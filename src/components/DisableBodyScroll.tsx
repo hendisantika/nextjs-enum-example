8f06f6b398f0f0846de5a4abe66ab2e7b85ef144
hendisantika <hendisantika@yahoo.co.id>
2024-09-12 18:18:23 +0700

    feat: Add DisableBodyScroll.tsx 🫸🌀✏️📗🐧🐳⬆

---
+++ src/components/DisableBodyScroll.tsx
@@ -0,0 +1,18 @@
+// @/components/DisableBodyScroll.js
+import {Component} from 'react';
+
+class DisableBodyScroll extends Component {
+    componentDidMount() {
+        document.body.classList.add('scroll-locked');
+    }
+
+    componentWillUnmount() {
+        document.body.classList.remove('scroll-locked');
+    }
+
+    render() {
+        return false;
+    }
+}
+
+export default DisableBodyScroll;
